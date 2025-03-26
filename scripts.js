2382b7d102a083bd54a1399c7e6fc456b7552fa1
kekel007 <kellvenmiedes21@gmail.com>
2025-03-26 19:19:24 -0300

    atualizando o valores das moedas

--- scripts.js
+++ scripts.js
@@ -1,19 +1,17 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
-function convertValues() {
+function convertValues () {
 
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const valueconverted = document.querySelector(".value-converted")
     const value = document.querySelector(".value")
+    
 
-    console.log(currencySelect.value)
-
-
-    const dolarToday = 6.19
-    const euroToday = 6.46
-    const libraToday = 7.79
-    const bitcoinToday = 135000
+    const dolarToday = 5.73
+    const euroToday = 6.16
+    const libraToday = 7.357
+    const bitcoinToday = 87.492
 
 
     if (currencySelect.value == "dolar") {
